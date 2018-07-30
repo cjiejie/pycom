ea5ba25383eba30065884a9e0fd41a58628cc862
cjiejie <1096448857@qq.com>
2018-07-30 18:33:39 +0800

    Update test.py

--- test.py
+++ test.py
@@ -2,8 +2,16 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from mainwindow import MainWindow
 import sys
 
+
+tmp_str = "OK"
+
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     mainWindow = MainWindow()
     mainWindow.show()
+
+    mainWindow.ThreadStartUp()
+
     sys.exit(app.exec_())
+
+
