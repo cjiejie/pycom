cbb9f04bac3d91b1c4784f2448c39d2d9e9e5019
cjiejie <1096448857@qq.com>
2018-07-30 18:32:11 +0800

    Update mainwindow.py

--- mainwindow.py
+++ mainwindow.py
@@ -2,6 +2,8 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from ui_my import Ui_MainWindow
 import sys
 import serial.tools.list_ports
+import myhandle
+import threading
 
 com_on_off = False
 wifi_on_off = False
@@ -13,11 +15,15 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         global port_list
         super(MainWindow, self).__init__(parent)
+
+        self.handle = myhandle.ComClass()
+
         self.setupUi(self)
+        # self.obj_get_data.setMaximumBlockCount(100)
         self.obj_get_data.setText("wait...")
 
         for item in port_list:
-            self.obj_com_port.addItem(str(item))
+            self.obj_com_port.addItem(str(item[0]))
         for item in ["9600", "38400", "115200"]:
             self.obj_com_baud.addItem(item)
 
@@ -26,25 +32,39 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.obj_recv_clear.clicked.connect(self.WindowClearRecv)
         self.obj_send_cmd.clicked.connect(self.WindowSendDataCmd)
 
-        self.obj_sta.textChanged.connect(self.WindowStu)
+    def ThreadStartUp(self):
+        self.handle.ThreadStart()
 
-    def WindowGetData(self):
-        self.obj_get_data.setText("wait...2")
+    def WindowGetData(self,str):
+        self.obj_get_data.append(str)
 
     def WindowSendData(self):
         self.obj_send_data.setText("...")
 
     def WindowComPower(self):
         global com_on_off
-        print(com_on_off)
+        print(com_on_off,sys._getframe().f_lineno)
         if com_on_off:
-            self.obj_com_on_off.setText("打开")
-            com_on_off = False
-
-        else :
+            myhandle.ComClass.CloseCom(self.handle)
             print("here is :", sys._getframe().f_lineno)
-            self.obj_com_on_off.setText("关闭")
-            com_on_off = True
+            self.obj_com_on_off.setText("打开")
+            self.WindowStu("Serial closed!")
+            com_on_off = False
+        else:
+            port = self.obj_com_port.currentText()
+            baud = self.obj_com_baud.currentText()
+            print("here is :", sys._getframe().f_lineno)
+            com_on_off = myhandle.ComClass.OpenCom(self.handle, port, baud)
+            print("here is :", sys._getframe().f_lineno)
+            print(com_on_off, sys._getframe().f_lineno)
+            if com_on_off:
+                self.obj_com_on_off.setText("关闭")
+                self.WindowStu("Open Serial OK!")
+                com_on_off = True
+            else:
+                self.obj_com_on_off.setText("打开")
+                self.WindowStu("Open Serial Fail!")
+                com_on_off = False
 
     def WindowWifiPower(self):
         global wifi_on_off
@@ -66,6 +86,12 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def WindowSendDataCmd(self):
         print("here is :", sys._getframe().f_lineno)
 
+    def WindowGetData(self, p_str):
+        self.obj_get_data.setText(p_str)
 
-    def WindowStu(self,p_str):
-        self.obj_sta.setText("OK")
+    def WindowSendData(self, p_str):
+        self.obj_send_data.setText(p_str)
+
+    def WindowStu(self, p_str):
+        self.obj_sta.setText(p_str)
+
